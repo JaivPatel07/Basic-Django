81e919da95cf56046380a856a489bddaccfeba48
Jaiv Patel <jaivpatel402@gmail.com>
2026-02-27 19:30:51 +0530

    Add dynamic content rendering to app1 page; fetch Employee details for display

--- app1/views.py
+++ app1/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Employee
 
 # import things we initially we dont have urls.py in app so we need to create urls.py in app1.
 
 # Create your views here.
 def app1(request):
-    return render(request,'app1.html')
+    temp = Employee.objects.all()
+    return render(request,'app1.html',{'details':temp})
